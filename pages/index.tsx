e6291a38f9198e8ec9621ba65fed4de5abe48cef
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2023-01-27 17:07:51 -0800

    removed GA

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,22 +52,6 @@ export default function Home({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <div className="container">
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-LSPH19721C');
-        `}
-          </Script>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-LSPH19721C"
-            strategy="afterInteractive"
-          />
-        </div>
       </Head>
 
       <main className="flex flex-col items-center">
