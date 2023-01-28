09aa45445bda43664d5ec756af39ea7ed77d122b
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2023-01-27 16:53:10 -0800

    testing

--- components/FeedbackForm.tsx
+++ components/FeedbackForm.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 function FeedbackForm() {
   return (
-    <Link href="https://twitter.com/YourBuddyConner?s=20&t=tuGKMQwm6Fb-mT5k-E87xw">
+    <Link href="https://twitter.com/YourBuddyConner">
       <p className="text-center text-xxs leading-5 text-gray-500 hover:text-gray-900 hover:underline">
         Got Feedback?
       </p>
